bafab093530f75b1b02216caf85e171eed4b90a6
Harshita <harshita4420@gmail.com>
2023-04-28 23:52:55 +0530

    Adding equals() reference comparison

---
+++ equalsMethod/Comparison.java
@@ -0,0 +1,14 @@
+package equalsMethod;
+
+public class Comparison {
+
+	public static void main(String[] args) {
+
+		Student st1 =  new Student();
+		Student st2 =  new Student();
+	
+		System.out.println(st1==st2);
+		System.out.println(st1.equals(st2));
+	}
+
+}
---
+++ equalsMethod/Student.java
@@ -0,0 +1,16 @@
+package equalsMethod;
+
+public class Student {
+	int x=34;
+
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println("Comparing objects");
+		
+		Student st = (Student) obj;
+		
+		return st.x == this.x;
+	}
+	
+	
+}
